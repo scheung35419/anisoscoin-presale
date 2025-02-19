e4b74aeace53b67bf813109562b5a7be4bbb2baf
scheung35419 <stevenklcheung@gmail.com>
2025-02-19 11:42:14 +0800

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,31 +1,23 @@
-{
-  "name": "anisoscoin-presale",
-  "version": "1.0.0",
-  "private": true,
-  "dependencies": {
-    "react": "^18.2.0",
-    "react-dom": "^18.2.0",
-    "react-scripts": "5.0.1",
-    "@solana/web3.js": "^1.41.0",
-    "@solana/wallet-adapter-react": "^0.15.35",
-    "@solana/wallet-adapter-react-ui": "^0.9.35",
-    "@solana/wallet-adapter-wallets": "^0.19.32"
-  },
-  "devDependencies": {
-    "react-app-rewired": "^2.2.1",
-    "crypto-browserify": "^3.12.0",
-    "stream-browserify": "^3.0.0",
-    "assert": "^2.0.0",
-    "stream-http": "^3.2.0",
-    "https-browserify": "^1.0.0",
-    "os-browserify": "^0.3.0",
-    "url": "^0.11.0",
-    "buffer": "^6.0.3"
-  },
-  "scripts": {
-    "start": "react-app-rewired start",
-    "build": "react-app-rewired build",
-    "test": "react-app-rewired test",
-    "eject": "react-scripts eject"
-  }
-}
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    crypto: require.resolve('crypto-browserify'),
+    stream: require.resolve('stream-browserify'),
+    assert: require.resolve('assert'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify'),
+    url: require.resolve('url'),
+    buffer: require.resolve('buffer')
+  });
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]);
+  return config;
+};
