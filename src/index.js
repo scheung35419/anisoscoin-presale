9759ba4b0e86b89f750060ea31ed334b7b5bce9b
scheung35419 <stevenklcheung@gmail.com>
2025-02-19 11:13:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import ReactDOM from 'react-dom';
 import Presale from './Presale';
-import { WalletProvider } from '@solana/wallet-adapter-react';
 
-const App = () => {
-  return (
-    <WalletProvider>
+// Solana Wallet Adapter
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+const network = WalletAdapterNetwork.Mainnet;
+const endpoint = clusterApiUrl(network);
+
+const wallets = useMemo(
+  () => [
+    new PhantomWalletAdapter(),
+  ],
+  [network]
+);
+
+ReactDOM.render(
+  <ConnectionProvider endpoint={endpoint}>
+    <WalletProvider wallets={wallets} autoConnect>
       <Presale />
     </WalletProvider>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+  </ConnectionProvider>,
+  document.getElementById('root')
+);
